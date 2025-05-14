43f652d1acedaf9e4429331a26bdae55afcf8de7
Anton <antonyupkodm@gmail.com>
2025-05-14 10:37:08 +0300

    feat: setup server, architecture

---
+++ backend/src/index.ts
@@ -0,0 +1,35 @@
+import Fastify from "fastify";
+import dotenv from "dotenv";
+import cors from "@fastify/cors";
+import { configureRoutes } from "./routes";
+
+dotenv.config();
+
+const fastify = Fastify({
+  logger: true,
+});
+
+const start = async () => {
+  try {
+    await fastify.register(cors, {
+      origin: true,
+      credentials: true,
+    });
+
+    configureRoutes(fastify);
+
+    await fastify.listen({
+      port: Number(process.env.PORT) || 3000,
+      host: "0.0.0.0",
+    });
+
+    console.log(
+      `🚀 Server ready at http://localhost:${process.env.PORT || 3000}`
+    );
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ backend/src/routes/weather/weather.routes.ts
@@ -0,0 +1,5 @@
+import { FastifyInstance } from "fastify";
+
+export const weatherRoutes = async (fastify: FastifyInstance) => {
+  
+};
